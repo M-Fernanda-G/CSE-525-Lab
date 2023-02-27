34bcdc1c9c16947b655f783f9da25e73cdf9ad45
M-Fernanda-G <M.Fernanda.G@outlook.com>
2023-02-27 13:02:26 -0500

    guh

--- Lesson02_Question2/src/mini_uart.c
+++ Lesson02_Question2/src/mini_uart.c
@@ -4,7 +4,8 @@
 
 void uart_send ( char c )
 {
-	while(1) {
+	while(1) 
+   {
 		if(get32(AUX_MU_LSR_REG)&0x20)
 			break;
 	}
@@ -13,7 +14,8 @@ void uart_send ( char c )
 
 char uart_recv ( void )
 {
-	while(1) {
+	while(1) 
+   {
 		if(get32(AUX_MU_LSR_REG)&0x01)
 			break;
 	}
@@ -22,7 +24,8 @@ char uart_recv ( void )
 
 void uart_send_string(char* str)
 {
-	for (int i = 0; str[i] != '\0'; i ++) {
+	for (int i = 0; str[i] != '\0'; i ++) 
+   {
 		uart_send((char)str[i]);
 	}
 }
@@ -53,7 +56,6 @@ void uart_init ( void )
 
 	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
 }
-
 
 // This function is required by printf function
 void putc ( void* p, char c)
